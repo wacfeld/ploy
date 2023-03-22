0c32076701cc8539d99c31c223826d345af8a667
daniel yu <wacfeldwang333@gmail.com>
2023-03-22 15:23:13 -0400

    cleanup main()

--- main.cc
+++ main.cc
@@ -11,23 +11,6 @@ int main(int argc, char **argv)
 {
   defaults();
   the_empty_list = new Sexp{false};
-  // Token T;
-  // while(nexttoken(std::cin, T)) {
-  //   std::cout << "  " << T << std::endl;
-  // }
 
   repl();
-
-  // std::vector<Token> toks = read_tokens(std::cin);
-  // toks = expand_quote(toks);
-  // // for(Token &T : toks) {
-  // //   std::cout << "  " << T << std::endl;
-  // // }
-  // // std::cout << toks << std::endl;
-  
-  // Sexp *e = get_sexp(toks);
-  // std::cout << e << std::endl;
-  // Sexp *f = eval(e);
-  // std::cout << f << std::endl;
-  // // std::cout << list_len(e) << std::endl;
 }
