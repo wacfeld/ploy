9638c2e7cd1365c9c2f391f7d6fd6cfad3290ce1
daniel yu <wacfeldwang333@gmail.com>
2023-03-22 15:38:27 -0400

    check_length()

--- forms.cc
+++ forms.cc
@@ -18,17 +18,31 @@ bool is_form(Sexp *e)
   return forms.count(e->a.symb);
 }
 
+void check_length(std::string &f, Sexp *e, int len) {
+  int ll = list_len(e);
+  if(ll != len) {
+    std::cerr << f << " requires " << len << " argument(s); received " << ll << std::endl;
+    longjmp(repl_start, 1);
+  }
+}
+
 Sexp *eval_form(Sexp *e)
 {
   std::string &f = e->car->a.symb;
 
   if(f == "quote") {
-    if(list_len(e->cdr) != 1) {
-      std::cerr << f << " requires 1 argument\n";
-    }
+    // if(list_len(e->cdr) != 1) {
+    //   std::cerr << f << " requires 1 argument\n";
+    //   longjmp(repl_start, 1);
+    // }
+    check_length(f, e->cdr, 1);
 
     return e->cdr->car;
   }
+
+  // else if(f == "if") {
+  //   if(list_len(
+  // }
 
   else {
     std::cerr << "don't recognize syntactic form " << f << std::endl;
