8a75a7e6c7de9ed0be172aa27929b01440aa32de
daniel yu <wacfeldwang333@gmail.com>
2023-03-22 10:09:39 -0400

    forms

---
+++ forms.cc
@@ -0,0 +1,36 @@
+#include "forms.h"
+
+// syntactic forms are quote, if, begin, cond, etc.
+std::set<std::string> forms {
+  "quote",
+};
+
+// true if e is syntactic form
+bool is_form(Sexp *e)
+{
+  if(!e->atom)
+    return false;
+
+  if(e->a.type != SYMBOL)
+    return false;
+
+  return forms.count(e->a.symb);
+}
+
+Sexp *eval_form(Sexp *e)
+{
+  std::string &f = e->car->a.symb;
+
+  if(f == "quote") {
+    if(list_len(e->cdr) != 1) {
+      std::cerr << f << " requires 1 argument\n";
+    }
+
+    return e->cdr->car;
+  }
+
+  else {
+    std::cerr << "don't recognize syntactic form " << f << std::endl;
+    exit(1);
+  }
+}
---
+++ forms.h
@@ -0,0 +1,13 @@
+#ifndef FORMS_H
+#define FORMS_H
+
+#include <set>
+#include <string>
+
+#include "sexp.h"
+
+extern std::set<std::string> forms;
+bool is_form(Sexp *e);
+Sexp *eval_form(Sexp *e);
+
+#endif
