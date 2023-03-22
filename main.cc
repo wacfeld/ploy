ecd398420633bf53d860e4040f5bc036f82428f3
daniel yu <wacfeldwang333@gmail.com>
2023-03-22 14:53:56 -0400

    repl()

--- main.cc
+++ main.cc
@@ -6,6 +6,23 @@
 #include "io.h"
 #include "defaults.h"
 
+void repl()
+{
+  while(true) {
+    std::vector<Token> toks = read_tokens(std::cin);
+    if(toks.empty()) {
+      std::cerr << "bye\n";
+      exit(0);
+    }
+
+    toks = expand_quote(toks);
+    Sexp *e = get_sexp(toks);
+    Sexp *f = eval(e);
+    std::cout << f << std::endl;
+  }
+  
+}
+
 int main(int argc, char **argv)
 {
   defaults();
@@ -15,16 +32,18 @@ int main(int argc, char **argv)
   //   std::cout << "  " << T << std::endl;
   // }
 
-  std::vector<Token> toks = read_tokens(std::cin);
-  toks = expand_quote(toks);
-  // for(Token &T : toks) {
-  //   std::cout << "  " << T << std::endl;
-  // }
-  // std::cout << toks << std::endl;
+  repl();
+
+  // std::vector<Token> toks = read_tokens(std::cin);
+  // toks = expand_quote(toks);
+  // // for(Token &T : toks) {
+  // //   std::cout << "  " << T << std::endl;
+  // // }
+  // // std::cout << toks << std::endl;
   
-  Sexp *e = get_sexp(toks);
-  std::cout << e << std::endl;
-  Sexp *f = eval(e);
-  std::cout << f << std::endl;
-  // std::cout << list_len(e) << std::endl;
+  // Sexp *e = get_sexp(toks);
+  // std::cout << e << std::endl;
+  // Sexp *f = eval(e);
+  // std::cout << f << std::endl;
+  // // std::cout << list_len(e) << std::endl;
 }
